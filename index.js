3eec8278a6160a889a04cc7995d201383ef8f976
Ankcorn <thomasankcorn@gmail.com>
2020-10-23 03:23:09 +0100

    add basic error handling and di support

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
+function parseJSON(value) {
+	try { 
+		return JSON.parse(value)
+	} catch(e) {
+		return value
+	}
+}
 
-
-export function jsonapi({ handler, cors }) {
+export function jsonapi({ handler, cors, dependencies, errors }) {
 	return async (event, context) => {
-		
-		const resp = await handler();
+		try {
+			const resp = await handler({ ...event, body: parseJSON(event.body) }, { ...context, ...dependencies && dependencies(event) });
 
-		const headers = cors && {
-      'Access-Control-Allow-Origin': cors,
-      'Access-Control-Allow-Credentials': true,
-		};
-		
-		if(typeof resp === 'string' || typeof resp === 'number') {
+			const headers = cors && {
+				'Access-Control-Allow-Origin': cors,
+				'Access-Control-Allow-Credentials': true,
+			};
+			
+			if(typeof resp === 'string' || typeof resp === 'number') {
+				return {
+					statusCode: 200,
+					...headers && { headers },
+					body: resp
+				}
+			}
+
 			return {
 				statusCode: 200,
 				...headers && { headers },
-				body: resp
+				body: JSON.stringify(resp)
+			}
+		} catch (e) {
+			if(errors) {
+				const key = Object.keys(errors).find(condition => RegExp(condition).test(e.message));
+				const resp = errors[key](e)
+				return {
+					...resp,
+					body: JSON.stringify(resp.body)
+				}
+			}
+			return {
+				statusCode: 500,
+				body: JSON.stringify({ message: 'Internal Server Error' })
 			}
 		}
-
-		return {
-			statusCode: 200,
-			...headers && { headers },
-			body: JSON.stringify(resp)
-		}
+		
 	}
 }
--- tests/jsonapi.test.js
+++ tests/jsonapi.test.js
@@ -1,6 +1,7 @@
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 import { jsonapi } from '../index';
+import event from './seed/event.json'
 
 test('beedi calls my handler and parses json response', async () => {
 	const handler = async() => {
@@ -12,12 +13,12 @@ test('beedi calls my handler and parses json response', async () => {
 		handler
 	});
 
-	const response = await lambda();
+	const response = await lambda(event);
 
 	assert.equal(response, {
 		statusCode: 200,
 		body: '{"message":"ok"}'
-	}, 'expects correct lambda response')
+	})
 })
 
 test('beedi calls my handler and parses string response', async () => {
@@ -28,12 +29,12 @@ test('beedi calls my handler and parses string response', async () => {
 		handler
 	});
 
-	const response = await lambda();
+	const response = await lambda(event);
 
 	assert.equal(response, {
 		statusCode: 200,
 		body: 'ok'
-	}, 'expects correct lambda response')
+	})
 });
 
 test('beedi calls my handler and parses number response', async () => {
@@ -44,12 +45,12 @@ test('beedi calls my handler and parses number response', async () => {
 		handler
 	});
 
-	const response = await lambda();
+	const response = await lambda(event);
 
 	assert.equal(response, {
 		statusCode: 200,
 		body: 4
-	}, 'expects correct lambda response')
+	})
 });
 
 test('beedi supports cors', async () => {
@@ -63,7 +64,7 @@ test('beedi supports cors', async () => {
 		cors: '*'
 	});
 
-	const response = await lambda();
+	const response = await lambda(event);
 
 	assert.equal(response, {
 		statusCode: 200,
@@ -72,7 +73,89 @@ test('beedi supports cors', async () => {
 			"Access-Control-Allow-Origin":"*",
 			"Access-Control-Allow-Credentials":true
 		}
-	}, 'expects correct lambda response')
+	})
+});
+
+
+
+test('beedi parses body to object to make life slightly more convenient', async () => {
+	const handler = async (event) => {
+		return event.body.message
+	}
+
+	const lambda = jsonapi({
+		handler,
+	});
+
+	const response = await lambda(event);
+
+	assert.equal(response, {
+		statusCode: 200,
+		body: "this a key of body in the test object",
+	})
+});
+
+test('beedi lets you inject dependencies to make testing easier', async () => {
+	const handler = async (event, { dep1, dep2 }) => {
+		return {
+			dep1: dep1(),
+			dep2: dep2(),
+		}
+	}
+
+	const lambda = jsonapi({
+		handler,
+		dependencies: () => ({ dep1: () => 'dep1 was called', dep2: () => 'dep2 was called' })
+	});
+
+	const response = await lambda(event, {});
+
+	assert.equal(response.statusCode, 200)
+	assert.equal(response.body, JSON.stringify({dep1:'dep1 was called',dep2:'dep2 was called'}));
+});
+
+test('beedi json api handles errors for you', async () => {
+	const handler = async (event, { dep1, dep2 }) => {
+		throw Error('big bad error')
+	}
+
+	const lambda = jsonapi({
+		handler,
+	});
+
+	const response = await lambda(event, {});
+
+	assert.equal(response, {
+		statusCode: 500,
+		body: "{\"message\":\"Internal Server Error\"}",
+	})
+});
+
+test('beedi json api can catch and configure errors with regex', async () => {
+	const handler = async (event, { dep1, dep2 }) => {
+		throw Error('big bad error')
+	}
+
+	const lambda = jsonapi({
+		handler,
+		errors: {
+			"big*": error => ({
+				body: { foo: error.message },
+				statusCode: 400,
+			}),
+			"*": error => ({
+				body: { message: 'Internal Server Error'},
+				statusCode: 500
+			})
+		}
+	});
+
+	const response = await lambda(event, {});
+
+	assert.equal(response, {
+		statusCode: 400,
+		body: "{\"foo\":\"big bad error\"}",
+	})
 });
 
 test.run();
